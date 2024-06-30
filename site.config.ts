ca91a79601dbf45c0bbc71caa588fa5797b1e406
inmare <yatsumehole@naver.com>
2024-06-30 11:47:48 +0900

    사이트 설정 변경

--- site.config.ts
+++ site.config.ts
@@ -1,17 +1,19 @@
+import { gSiteConfig } from 'g-vercel-app-config/site.config'
+
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: gSiteConfig.rootNotionPageId,
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: gSiteConfig.name,
+  domain: gSiteConfig.domain,
+  author: gSiteConfig.author,
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
