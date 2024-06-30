6ac08991ddb49bdbdb4dae880cc73d9705e0bb55
inmare <yatsumehole@naver.com>
2024-06-30 12:03:32 +0900

    submodule 폴더 추가 및 사이트 서

--- site.config.ts
+++ site.config.ts
@@ -16,12 +16,12 @@ export default siteConfig({
   author: gSiteConfig.author,
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  // description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  // github: 'transitive-bullshit',
+  // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
